cbb3e283acc404eebb475fbac453199c692416bd
mycomycul <32719514+mycomycul@users.noreply.github.com>
2019-01-06 20:17:16 -0800

    Add React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Panel from './components/Panel';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Panel></Panel>
+      </div>
+    )
+  }
+}
