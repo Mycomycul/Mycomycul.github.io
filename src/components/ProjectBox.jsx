4139478409baf14fd8cdb8b50238539cb241b077
mycomycul <32719514+mycomycul@users.noreply.github.com>
2019-01-16 13:59:33 -0800

    Map skillsas array

--- src/components/ProjectBox.jsx
+++ src/components/ProjectBox.jsx
@@ -3,22 +3,19 @@ import "./css/ProjectBox.css";
 
 class ProjectBox extends Component {
   render() {
+    const { id, project, onClick } = this.props;
     return (
       <div className="col-4 col-sm-6">
-        <div
-          className="project-box"
-          onClick={() => this.props.onClick(this.props.project.id)}
-        >
-          <title className="griditem project-title">
-            {this.props.project.title}
-          </title>
+        <div className="project-box" onClick={() => onClick(id)}>
+          <title className="griditem project-title">{project.title}</title>
           <section className="project-links grid-item">Links</section>
           <section className="project-description grid-item">
-            <span className="project-summary">
-              {this.props.project.summary}
-            </span>
+            <span className="project-summary">{project.summary}</span>
             <br />
-            <strong>Skills:</strong> {this.props.project.skills}
+            <strong>Skills: </strong>
+            {project.skills.map((skill, index) => (
+              <span key={index}>{skill} </span>
+            ))}
             <br />
           </section>
         </div>
