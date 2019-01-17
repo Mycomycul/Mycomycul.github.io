50b4193f154cc5e9c7142b8c47f659e921ccafee
mycomycul <32719514+mycomycul@users.noreply.github.com>
2019-01-16 16:45:31 -0800

    Add and Organize Modal Content

--- src/App.js
+++ src/App.js
@@ -7,34 +7,64 @@ import "./App.css";
 export default class App extends Component {
   state = {
     projectBox: [
-
       {
-        id: 0,
         title: "PDF Writer",
         summary:
-          "Take HTML form inputs and print them to a pre-existing PDF document",
-        skills: "C#, ASP.NET MVC, JSON,  Photoshop, ",
+          "Print HTML form inputs on a pre-existing PDF document",
+        skills: ["C#", "ASP.NET MVC", "JSON", "Photoshop"],
         links: [
-          { github: "https://github.com/mycomycul/PDFWriter", label: "Github" },
+          { link: "https://github.com/mycomycul/PDFWriter", label: "Github" },
           {
-            pdf:
-              "https://github.com/mycomycul/PDFWriter/blob/master/PrintedCatContract.pdf",
-            label: "Completed Contract"
+            link:
+              "https://github.com/mycomycul/PDFWriter/blob/master/FormScreenShot.jpg",
+            label: "Sample Form"
           },
           {
-            screenshot:
-              "https://github.com/mycomycul/PDFWriter/blob/master/FormScreenShot.jpg",
-            label: "Sample"
+            link:
+              "https://github.com/mycomycul/PDFWriter/blob/master/PrintedCatContract.pdf",
+            label: "Completed Contract"
           }
         ],
         description:
-          "This is a sample version of a larger project for digitally signing large PDF contracts. The company needed a solution that didn't require an API subscription or creating a new editable PDF. \n\n This sample is for registering your feline when moving to the moon. It recalculates your pet's weight for the moon, fills in the contract with the modfied content and also issues a registration certificate from the lunar authorities"
+          ["This is a sample version of a larger project for digitally signing large PDF contracts. The company needed a solution that didn't require an API subscription or creating a new editable PDF. \n\n This sample is for registering your feline when moving to the moon. It recalculates your pet's weight for the moon, fills in the contract with the modfied content and also issues a registration certificate from the lunar authorities. \n The project uses ASP.NET MVC and the PDFSharp Library"],
+          features:["Print to multiple page documents","Maintain original PDF formatting","Stores printing locations in JSON","Works with radiobuttons for PDF checkboxes","Handles font, font-size, and font-style","Email or direct download completed documents"]
       },
       {
-        id: 1,
-        title: "PDF Writer",
-        summary: "",
-        description: "Description. This is a description of the project"
+        title: "Strunktech.com",
+        summary: "Project portfolio website built with React",
+        skills: ["React.js", "HTML", "CSS", "JS"],
+        links:[{label:"Strunktech.com",link:"www.strunktech.com"}],
+        description:
+          ["This website is currently being transtioned to a React Product when I can spare time"]
+      },
+      {
+        title: "My Trails",
+        summary: "Trail Conditions Scraper and Map Visualizer",
+        skills: [
+          "C#",
+          "ASP.NET MVC",
+          "JS",
+          "GeoJSON",
+          "SQL Server Spatial Data",
+          "Google Maps"
+        ],
+        description:[ "Scrapes trail conditons from the Olmypic National Park website so that they can viewed alongside a map of trail conditions.",
+      "The current website is a static website with no long term records or visual UX.  MyTrails allows you to look at a history of conditions and the location of selected trails on a map"],
+      features:["Imports GeoJSON data into SQL Server and returns it back as JSON", "Web Scraping of NPS Trails Conditions","Cleans content of excess HTML markup", "Automatic and Manual pairing of trail conditions with map data","Interface for viewing trail names, a long-term record of conditions and map data"],
+      indev : "Build react interface, Oraganize pages"
+
+      },
+
+      {
+        title: "MSDN Resize",
+        summary: "Chrome MSDN restyling extension for smaller screens ",
+        skills: [
+          "Google Chrome Extensions", "JS", "CSS", "HTML"
+        ],
+        image:"MSDNResize.png",
+        links:[{label:"Chrome Store",link:"https://chrome.google.com/webstore/detail/msdn-resize/bdcaffgeeafpmnpeogoipgfhklbppjlc"},{label:"Github",link:"https://github.com/mycomycul/MSDNFix"}],
+        description: ["Google Chrome Extension that allows the user to reduce the the width of the MSDN side bar. If you've ever tried to use the MSDN Documentaiton on half your screen and found too much screen real estate was dedicated to the sidebar?"],
+        features:["Adds Vertical divider between panes for quick resizing with just a click", "Automatically resizes with window resizing"]
       }
     ],
     modalProject: {}
@@ -46,16 +76,16 @@ export default class App extends Component {
   };
   handleCloseModal = () => {
     const modalProject = {};
-    this.setState({modalProject: modalProject});
+    this.setState({ modalProject: modalProject });
   };
   handleWindowClick = event => {
-    if(event.target == this.modal){
+    if (event.target == this.modal) {
       this.handleCloseModal();
     }
-  }
-  componentDidMount(){
-    this.modal = document.getElementById('project-modal');
-    window.addEventListener('click',this.handleWindowClick);
+  };
+  componentDidMount() {
+    this.modal = document.getElementById("project-modal");
+    window.addEventListener("click", this.handleWindowClick);
   }
   render() {
     return (
@@ -63,9 +93,11 @@ export default class App extends Component {
         <Panel
           projectBox={this.state.projectBox}
           onClick={this.handleOpenModal}
-
         />
-        <Modal project={this.state.modalProject} onClose={this.handleCloseModal}/>
+        <Modal
+          project={this.state.modalProject}
+          onClose={this.handleCloseModal}
+        />
       </React.Fragment>
     );
   }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,21 +1,76 @@
 import React, { Component } from "react";
 import "./css/Modal.css";
-
+function intersperse(arr, sep) {
+  if (arr.length !== 0) {
+    return arr.slice(1).reduce(
+      function(xs, x, i) {
+        return xs.concat([sep, x]);
+      },
+      [arr[0]]
+    );
+  }
+  return [];
+}
 export default class Modal extends Component {
   render() {
     //If no data is received, clear the modal and close it
-    const { description = "", display = false } = this.props.project || {};
+    const {
+      skills = [],
+      description = [],
+      display = false,
+      links = [],
+      title = "",
+      image = "",
+      summary = "",
+      features = [],
+      indev = ""
+    } = this.props.project || {};
+    var sepskills = intersperse(skills, ",");
+
     return (
       <div id="project-modal" className={this.getDisplayClass(display)}>
         <div className="modal-content">
           <span onClick={this.props.onClose} className="modal-close">
             &times;
           </span>
-          {/* Breaks apart text on newlines (\n) and puts them each in a new span */}
-          {description.split("\n").map((item, key) => {
-            return <p key={key}>{item}</p>;
-          })}
-          {/* <p>{description}</p> */}
+          {image != "" && (
+            <img className="project-pic" src={"./images/projects/" + image} />
+          )}
+          <h3 className="modal-project-title">{title}</h3>
+          {/* DESCRIPTIONS Breaks apart text on newlines (\n) and puts them each in a new span */}
+          {description.map((line, index) => (
+            <section key={index}>
+              {" "}
+              {line.split("\n").map((item, key) => {
+                return <p key={key}>{item}</p>;
+              })}
+            </section>
+          ))}
+          <ul>
+            {features.map((feature, index) => (
+              <li key={index}>{feature} </li>
+            ))}
+          </ul>
+          {indev && (
+            <p className="">
+              <h4 className="in-dev">In DEV:</h4> <span>{indev}</span>
+            </p>
+          )}
+          <section>
+            {sepskills.map((skill, index) => (
+              <span key={index}>{skill} </span>
+            ))}
+          </section>
+          <hr />
+          <section className="modal-links">
+            {links.map((link, key) => {
+              return (
+                <a href={link.link} key={key[0]}>
+                  {link.label}
+                </a>
+              );
+            })}
+          </section>
         </div>
       </div>
     );
