cbd2d958fbfe4c7ec4beca04213a14ade77b7d67
mycomycul <32719514+mycomycul@users.noreply.github.com>
2019-01-23 22:15:34 -0800

    Add skills svg icons

--- src/components/icon.jsx
+++ src/components/icon.jsx
@@ -1,22 +1,41 @@
 import React, { Component } from "react";
 import symboldefs from "./css/symbol-defs.svg";
+import "./css/icon.css";
 
 const icons = {
-  HTML: { icon: "icon-html-five" },
-  "React.js": { icon: "icon-react" },
-  CSS: { icon: "icon-css3" },
-  JS: { icon: "icon-javascript" }
+  HTML: { icon: "html5" },
+  "React.js": { icon: "react" },
+  CSS: { icon: "css3" },
+  JS: { icon: "javascript" },
+  "ASP.NET": { icon: "dot-net" },
+  Python: { icon: "python" },
+  "Raspberry Pi": { icon: "raspberrpi" },
+  Photoshop: { icon: "adobephotoshop" },
+  "Google Chrome Extensions": { icon: "chrome" }
 };
 
 export default class icon extends Component {
   render() {
-    const { icon } = this.props;
-    return icons[icon] ? (
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
-        <use xlinkHref={`${symboldefs}#${icons[icon].icon}`} />
-      </svg>
-    ) : (
-      <span>{icon}</span>
-    );
+    const { icon, displayType } = this.props;
+
+    if (displayType != "text" && icons[icon]) {
+      return renderIcon(icon);
+    } else if (displayType != "icon") {
+      return renderText(icon);
+    }
+    return null;
   }
+}
+function renderIcon(icon) {
+  return (
+    <i className="icon">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+        <use xlinkHref={`${symboldefs}#${"icon-" + icons[icon].icon}`} />
+      </svg>
+    </i>
+  );
+}
+
+function renderText(icon) {
+  return <span>{icon}</span>;
 }
