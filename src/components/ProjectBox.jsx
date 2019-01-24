a71cd14e6e49c968c3938b679c3255bc92bc341f
mycomycul <32719514+mycomycul@users.noreply.github.com>
2019-01-23 22:16:55 -0800

    Add Icons to Project Box

--- src/components/ProjectBox.jsx
+++ src/components/ProjectBox.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import "./css/ProjectBox.css";
+import Icon from "./icon";
+import intersperse from "./function.js";
 
 class ProjectBox extends Component {
   render() {
-    const { id, project, onClick } = this.props;
+    const { id, project, onClick } = this.props || {};
+    const sepskills = intersperse(this.props.project.skills, ", ");
     return (
       <div className="col-4 col-sm-6">
         <div className="project-box" onClick={() => onClick(id)}>
@@ -12,9 +15,16 @@ class ProjectBox extends Component {
           <section className="project-description grid-item">
             <span className="project-summary">{project.summary}</span>
             <br />
-            <strong>Skills: </strong>
-            {project.skills.map((skill, index) => (
+
+            {/* {project.skills.map((skill, index) => (
               <span key={index}>{skill} </span>
+            ))} */}
+            {project.skills.map((skill, index) => (
+              <Icon key={index} icon={skill} displayType="icon" />
+            ))}
+            <br />
+            {sepskills.map((skill, index) => (
+              <Icon key={index} icon={skill} displayType="text" />
             ))}
             <br />
           </section>
